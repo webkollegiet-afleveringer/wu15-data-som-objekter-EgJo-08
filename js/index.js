ea614b44ab990d7fe24fce92b770d16a4065e41e
EgJo-08 <U2535495@rts-365.dk>
2026-02-04 09:34:03 +0100

    har lavet number 2 ved at lave en foreach og gøre det samme som I den samme inde i foreach lykken

--- js/index.js
+++ js/index.js
@@ -23,7 +23,41 @@ let icon2 = document.createElement("img")
 icon2.src = hero.icon
 first.append(icon2)
 
+let services2 = document.querySelector(".services")
 
+services.forEach(service => {
+    const div = document.createElement("div")
+    let illustration = document.createElement("img")
+
+    illustration.src = service.images
+
+    div.appendChild(illustration)
+
+    services2.appendChild(div)
+
+    let headline = document.createElement("h2")
+
+    headline.textContent = service.headline
+
+    div.appendChild(headline)
+    services2.appendChild(div)
+
+    let text = document.createElement("p")
+
+    text.textContent = service.text
+
+    div.appendChild(text)
+
+    services2.appendChild(div)
+
+    let linktext = document.createElement("a")
+
+    linktext.textContent = service.linktext
+    linktext.href = service.url
+
+    div.appendChild(linktext)
+
+})
 
 
 services.forEach(service => console.log(service.headline))
