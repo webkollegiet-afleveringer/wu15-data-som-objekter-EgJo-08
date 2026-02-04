a0d49f358b10c3317711d91ddf8f2a7b9ee17997
EgJo-08 <U2535495@rts-365.dk>
2026-02-04 09:09:03 +0100

    har lavet den første variabel med at bare bruge append og lave nogle variabler til billederne

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ let hero = {
     image: "https://dazzling-squirrel-77a63d.netlify.app/imgs/hero.png",
     headline: "Find out how we can save your time in Backpacking in Europe",
     copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur.",
-    icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Globe.png"
+    icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/hero.png"
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-// slå dig løs her... 
+let first = document.querySelector(".hero")
 
-//eksempel på at udskrive alle overskrifter i services i konsollen:
+function hello() {
+    first.addElement("img")
+
+}
+
+let img = document.createElement("img")
+img.src = hero.image
+first.append(img)
+
+
+first.append(hero.headline)
+first.append(hero.copy)
+
+let icon2 = document.createElement("img")
+icon2.src = hero.icon
+first.append(icon2)
+
+
+
+
 services.forEach(service => console.log(service.headline))
